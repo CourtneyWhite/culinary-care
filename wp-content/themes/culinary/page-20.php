aaa3e948bc5dc2ebfc69a638856d09dd084b74ef
Brad Sawicki <git@bradsawicki.com>
2014-12-04 17:38:18 -0600

    Ways to Give styles

---
+++ wp-content/themes/culinary/page-20.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<section class="main">
+	  <div class="wrapper">
+
+		<h1 class="page-title"><?php the_title(); ?></h1>
+
+    <div class="body">
+  		<?php the_content(); ?>
+    </div>
+    
+    <div class="give-grid grid">
+      <div class="col-1-2">
+        <div class="give-image">
+          <img src="/wp-content/themes/culinary/assets/images/pic-give-01.jpg" alt="">
+        </div>
+  			<?php the_field('give_block_1'); ?>
+      </div>
+      <div class="col-1-2">
+        <div class="give-image">
+          <img src="/wp-content/themes/culinary/assets/images/pic-give-02.jpg" alt="">
+        </div>
+  			<?php the_field('give_block_2'); ?>
+      </div>
+      <div class="col-1-2">
+        <div class="give-image">
+          <img src="/wp-content/themes/culinary/assets/images/pic-give-03.jpg" alt="">
+        </div>
+  			<?php the_field('give_block_3'); ?>
+      </div>
+      <div class="col-1-2">
+        <div class="give-image">
+          <img src="/wp-content/themes/culinary/assets/images/pic-give-04.jpg" alt="">
+        </div>
+  			<?php the_field('give_block_4'); ?>
+      </div>
+    </div>
+
+	  </div>
+	</section>
+	<?php endwhile; endif; ?>
+
+<?php // get_sidebar(); ?>
+
+<?php get_footer(); ?>
--- wp-content/themes/culinary/page.php
+++ wp-content/themes/culinary/page.php
@@ -1,28 +1,18 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-		<article class="post" id="post-<?php the_ID(); ?>">
+	<section class="main">
+	  <div class="wrapper">
 
-			<h1 class="page-title"><?php the_title(); ?></h1>
+		<h1 class="page-title"><?php the_title(); ?></h1>
 
-			<?php // posted_on(); ?>
+    <div class="body">
+  		<?php the_content(); ?>
+    </div>
 
-			<div class="entry">
-
-				<?php the_content(); ?>
-
-				<?php // wp_link_pages(array('before' => __('Pages: ','html5reset'), 'next_or_number' => 'number')); ?>
-
-			</div>
-
-			<?php edit_post_link(__('Edit this entry','html5reset'), '<p>', '</p>'); ?>
-
-		</article>
-		
-		<?php // comments_template(); ?>
-
-		<?php endwhile; endif; ?>
+	  </div>
+	</section>
+	<?php endwhile; endif; ?>
 
 <?php // get_sidebar(); ?>
 
