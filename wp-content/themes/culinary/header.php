1b51493994d6ddb119a3fe8c5f1626be2614b8e9
Brad Sawicki <git@bradsawicki.com>
2014-12-17 15:02:52 -0600

    Viewport meta

--- wp-content/themes/culinary/header.php
+++ wp-content/themes/culinary/header.php
@@ -5,6 +5,7 @@
 	<!--[if IE ]>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<![endif]-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta name="title" content="<?php wp_title( '|', true, 'right' ); ?>">
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
