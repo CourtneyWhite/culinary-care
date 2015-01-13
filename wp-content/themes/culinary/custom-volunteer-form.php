9452ad35d4701c70131e9f7d44a0c44527aa5b9d
Brad Sawicki <git@bradsawicki.com>
2015-01-13 10:30:24 -0600

    Volunteer Form single col

--- wp-content/themes/culinary/custom-volunteer-form.php
+++ wp-content/themes/culinary/custom-volunteer-form.php
@@ -5,19 +5,18 @@ Template Name: Volunteer Form
 get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<section class="main">
+	<section class="main volunteer-form-page">
 	  <div class="wrapper">
 
-		<h1 class="page-title"><?php the_title(); ?></h1>
-
-    <div class="volunteer-form-grid grid">
-      <div class="col-1-2 body">
+  		<h1 class="page-title"><?php the_title(); ?></h1>
+  		
+  		<div class="body">
     		<?php the_content(); ?>
-      </div>
-      <div class="col-1-2 volunteer-form">
+  		</div>
+  
+      <div class="volunteer-form">
   			<?php the_field('volunteer_form'); ?>
       </div>
-    </div>
 
 	  </div>
 	</section>
